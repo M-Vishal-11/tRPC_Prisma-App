6f4238bc3366eab65654279ba920287f105531e7
M-Vishal-11 <m.vishal9a@gmail.com>
2026-05-29 13:15:07 +0530

    Created my First Context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { useTRPC } from "./_trpc/client";
-import { stringify } from "querystring";
 
 export default function Home() {
   const trpc = useTRPC();
@@ -14,7 +13,7 @@ export default function Home() {
         Get The Data from backend or the Server
       </h1>
       <div className="mt-15 border-2 border-amber-400 m-50 p-10">
-        <p className="text-xl">Data: {!isLoading && stringify(data)}</p>
+        <p className="text-xl">Data: {!isLoading && JSON.stringify(data)}</p>
       </div>
     </div>
   );
--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,10 @@
 import { initTRPC } from "@trpc/server";
 
-const t = initTRPC.create();
+type Context = {
+  name: string;
+};
+
+const t = initTRPC.context<Context>().create();
 
 export const publicProcedure = t.procedure;
 export const router = t.router;

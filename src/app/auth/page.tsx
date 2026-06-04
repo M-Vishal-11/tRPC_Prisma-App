0958c53de9133ec37fce2f5ccf3e45770a2086c1
M-Vishal-11 <m.vishal9a@gmail.com>
2026-06-04 15:24:56 +0530

    Google Auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -150,6 +150,21 @@ export default function AuthenticationPage() {
     );
   };
 
+  const handleGoogleSignIn = async () => {
+    const data = await signIn.social(
+      {
+        provider: "google",
+        callbackURL: "/",
+      },
+      {
+        onError: (ctx) => {
+          console.log(ctx.error.message || "Something went wrong");
+          setError(ctx.error.message || "Something went wrong");
+        },
+      },
+    );
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8">
@@ -273,12 +288,45 @@ export default function AuthenticationPage() {
             </div>
 
             {!signedIn ? (
-              <button
-                type="submit"
-                className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition font-medium"
-              >
-                Login
-              </button>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition font-medium"
+                >
+                  Login
+                </button>
+                <button
+                  type="button"
+                  onClick={handleGoogleSignIn}
+                  className="mt-4 w-full flex items-center justify-center gap-3 bg-white text-gray-700 border border-gray-300 py-3 rounded-lg hover:bg-gray-50 active:scale-[0.99] transition font-medium shadow-sm"
+                >
+                  {/* Official Google Brand Logo */}
+                  <svg
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                      fill="#4285F4"
+                    />
+                    <path
+                      d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                      fill="#34A853"
+                    />
+                    <path
+                      d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.06H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.94l2.85-2.22.81-.63z"
+                      fill="#FBBC05"
+                    />
+                    <path
+                      d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z"
+                      fill="#EA4335"
+                    />
+                  </svg>
+                  Sign in with Google
+                </button>
+              </div>
             ) : (
               <button
                 onClick={handleOTPVerification}
@@ -333,6 +381,37 @@ export default function AuthenticationPage() {
               // disabled={!verified}
             >
               Create Account
+            </button>
+            <button
+              type="button"
+              onClick={handleGoogleSignIn}
+              className=" w-full flex items-center justify-center gap-3 bg-white text-gray-700 border border-gray-300 py-3 rounded-lg hover:bg-gray-50 active:scale-[0.99] transition font-medium shadow-sm"
+            >
+              {/* Official Google Brand Logo */}
+              <svg
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                  fill="#4285F4"
+                />
+                <path
+                  d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                  fill="#34A853"
+                />
+                <path
+                  d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.06H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.94l2.85-2.22.81-.63z"
+                  fill="#FBBC05"
+                />
+                <path
+                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z"
+                  fill="#EA4335"
+                />
+              </svg>
+              Sign in with Google
             </button>
           </form>
         )}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,14 @@ import { createAuthMiddleware } from "better-auth/api";
 import { twoFactor } from "better-auth/plugins";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
@@ -71,7 +79,7 @@ export const auth = betterAuth({
     }),
   ],
 
-  // trustedOrigins: ["http://localhost:3001"],
+  trustedOrigins: [process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3001"],
   hooks: {
     after: createAuthMiddleware(async (ctx) => {
       const newSession = ctx.context.newSession;
